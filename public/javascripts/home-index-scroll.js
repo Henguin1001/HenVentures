b9fa287e9d239567efd095e1916154b85f07f057
Henry Troutman <henguin1001@gmail.com>
2014-07-28 10:17:52 -0700

    typing text no longer waits till everything loads

--- public/javascripts/home-index-scroll.js
+++ public/javascripts/home-index-scroll.js
@@ -41,7 +41,7 @@ TxtRotate.prototype.tick = function() {
   }, delta);
 };
 
-window.onload = function() {
+function startRotation(){
   var elements = document.getElementsByClassName('txt-rotate');
   for (var i=0; i<elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-rotate');
@@ -55,9 +55,12 @@ window.onload = function() {
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #5FC1E8 }";
   document.body.appendChild(css);
-  forceCenter();
-};
+}
 
+
+$(document).ready(function(){
+  forceCenter();
+});
 $(window).resize(function(){
   forceCenter();
 });
